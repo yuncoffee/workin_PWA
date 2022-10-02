d7cb2c0bdb6df3b7a5ae09a88ad0cace34c1d855
yuncoffee <dong072815@gmail.com>
2022-10-03 01:53:35 +0900

    [chore] config 파일 무시

---
+++ src/config/firebase.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBPxIFSIHnoHsidFbEgyHuTW2_Lx8MJ2jI",
+    authDomain: "workin-46df7.firebaseapp.com",
+    projectId: "workin-46df7",
+    storageBucket: "workin-46df7.appspot.com",
+    messagingSenderId: "410492617599",
+    appId: "1:410492617599:web:4e8e1a20ad82936082504a",
+    measurementId: "G-9LECMBT1H8",
+}
